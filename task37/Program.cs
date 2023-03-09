2cc65c7c6727ee82ceb22794ff6011df36518201
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-09 22:27:47 +0300

    task37 solved

---
+++ task37/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+int[] convertArray(int[] arr)
+{
+    int[] result = new int[arr.Length / 2 + arr.Length % 2];
+    for(int i = 0; i < arr.Length / 2; i++)
+    {
+        int j = arr.Length - i - 1;
+        result[i] = arr[i] * arr[j];
+    }
+    if(arr.Length % 2 != 0)
+    {
+        result[result.Length - 1] = arr[arr.Length / 2];
+    }
+    return result;
+}
+int[] arr = { 6, 7, 3, 6 };
+int[] converted = convertArray(arr);
+Console.WriteLine(string.Join(" ", converted));
